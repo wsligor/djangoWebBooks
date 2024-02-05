9afeb4fc536201fe4304a986550f8f81563eb7f3
wsligor <wsligor@gmail.com>
2024-02-05 18:08:45 +0300

    base.html, index.html

--- catalog/views.py
+++ catalog/views.py
@@ -3,4 +3,10 @@ from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Главная страница каталога 'Мир книг'!!!'")
+    # Словарь для передачи данных в шаблон
+    text_head = 'Это заголовок главной страницы раздела Каталог'
+    text_body = 'Это содержимое главной страницы раздела Каталог'
+    context = {'text_head': text_head, 'text_body': text_body}
+    # передача словаря context с данными в шаблон
+    return render(request, 'index.html', context)
+    # return HttpResponse("Главная страница каталога 'Мир книг'!!!'")
