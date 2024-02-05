1169e84109f819ab48263d51a3effec9a5fe743f
wsligor <fornet11@mail.ru>
2024-02-05 22:15:14 +0300

    update catlog/views.ru update index.html

--- catalog/views.py
+++ catalog/views.py
@@ -1,12 +1,24 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from catalog.models import Book, BookInstance, Author
+
 
 def index(request):
     # Словарь для передачи данных в шаблон
-    text_head = 'Это заголовок главной страницы раздела Каталог'
-    text_body = 'Это содержимое главной страницы раздела Каталог'
-    context = {'text_head': text_head, 'text_body': text_body}
+    text_head = 'На нашем сайте вы можете получить книги в электроном виде'
+    # Данные об экземплярах книг в БД
+    books = Book.objects.all()
+    num_books = Book.objects.all().count()
+    # Данные об экземплярах книг в БД
+    num_instances = BookInstance.objects.all().count()
+    # Доступные книги (статус = "На складе")
+    num_instances_available = BookInstance.objects.filter(status__exact=2).count()
+    # Данные об авторах книг
+    authors = Author.objects
+    num_authors = Author.objects.count()
+    # Словарь для передачи данных в шаблон index.html
+    context = {'text_head': text_head, 'books': books, 'num_books': num_books, 'num_instances': num_instances,
+               'num_instances_available': num_instances_available, 'authors': authors, 'num_authors': num_authors}
     # передача словаря context с данными в шаблон
     return render(request, 'index.html', context)
-    # return HttpResponse("Главная страница каталога 'Мир книг'!!!'")
