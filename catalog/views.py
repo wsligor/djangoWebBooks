229b0629e6ff697b8ea16db5e9ed198e1af055e1
wsligor <wsligor@gmail.com>
2024-02-05 17:30:04 +0300

    Первоначальное определение urls

--- catalog/views.py
+++ catalog/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Главная страница сайта 'Мир книг'!!!'")
+    return HttpResponse("Главная страница каталога 'Мир книг'!!!'")
--- djangoWebBooks/urls.py
+++ djangoWebBooks/urls.py
@@ -15,10 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from catalog.views import *
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from .vievs import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index, name='index')
+    path('catalog/', include('catalog.urls')),
+    path('home/', home, name='home'),
+    path('', home, name='home'),
 ]
+
+if settings.DEBUG:
+    urlpatterns +=  static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
